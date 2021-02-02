c916cee8a9bdbb0aa07ddd484b7ed931f49750e9
shahirZain <shahirzain100@gmail.com>
2021-02-03 02:54:00 +0500

    signup added

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,6 @@ var app = express();
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/productData");
 
-
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
@@ -27,18 +26,67 @@ app.get("/", async function (req, res) {
   res.send(getDatas);
 });
 
-
-app.delete('/orderplaced/:id',async function (req, res) {
-  console.log("req.params.id :::",req.params.id);
-  let getDatas = await tempData.deleteOne({id : req.params.id},function(){
-    
-    console.log("data dltd")
+app.delete("/orderplaced/:id", async function (req, res) {
+  console.log("req.params.id :::", req.params.id);
+  let getDatas = await tempData.deleteOne({ id: req.params.id }, function () {
+    console.log("data dltd");
   });
   console.log(getDatas);
-  res.send("deleted Data")
-})
+  res.send("deleted Data");
+});
 
 
-app.listen(8080, () => {
+
+
+
+
+var bcrypt = require("bcrypt");
+var bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded;
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+
+var usersSchema = mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: String,
+  email: { type: String, required: true, unique: true },
+  password : { type: String, required: true },
+});
+
+usersSchema.pre("save", () => console.log("Hello from pre save"));
+//creating a model
+var User = mongoose.model("Users", usersSchema);
+
+app.post("/signup", function (req, res, next) {
+  console.log("Request Url:" + req.url);
+  console.log(req.body);
+
+  const { firstname, email, password } = req.body;
+  User.findOne({ email: req.body.email }, function (err, users) {
+    if (err) console.log(err);
+
+    // object of all the users
+
+    console.log("users",users);
+    if (users) {
+      res.status(408).send();
+    } else {
+      User.insertMany({
+        firstname,
+        email,
+        password,
+      });
+      usersSchema().save()
+      res.send("User has been Saved!!!");
+      console.log("User has been Saved!!!");
+    }
+  });
+});
+
+app.listen(8081, () => {
   console.log("server is up");
 });
