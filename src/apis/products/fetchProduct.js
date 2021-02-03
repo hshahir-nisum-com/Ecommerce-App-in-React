5c1c85ab6635aec0c2fc463cd218802140a832d0
hshahir-nisum-com <hshahir@nisum.com>
2021-02-03 11:12:15 +0500

    commited

--- src/apis/products/fetchProduct.js
+++ src/apis/products/fetchProduct.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 async function apiProduct() {
     const { data } = await axios.get("http://localhost:8080/");
-    console.log("from dataaaaaaaa", data)
     return data
   }
 
